65f2e8f4258311e2700419458c7bf773159cfb74
Karlox01 <orlandinikarlo@gmail.com>
2023-12-05 23:47:20 +0000

    refactored custom context hook

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
-axios.defaults.baseURL = 'https://momentstwo-faa9f7e8d370.herokuapp.com'
+axios.defaults.baseURL = 'https://djangoapitwo-c793e711fc8c.herokuapp.com/'
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
