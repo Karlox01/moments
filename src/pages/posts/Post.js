8d633b1a9be1b13bfd8b0732e7dc581593035f9f
Karlox01 <orlandinikarlo@gmail.com>
2023-12-15 21:47:51 +0000

    add ability to edit posts, add comments, delete comments

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -3,9 +3,11 @@ import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from '../../contexts/CurrentUserContext';
 import { Card, OverlayTrigger, Tooltip } from 'react-bootstrap';
 import Media from 'react-bootstrap/Media';
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import Avatar from '../../components/Avatar';
 import { axiosRes } from '../../api/axiosDefaults';
+import { MoreDropdown } from '../../components/MoreDropdown';
+
 
 const Post = (props) => {
     const {
@@ -26,6 +28,22 @@ const Post = (props) => {
 
     const currentUser = useCurrentUser();
     const is_owner = currentUser?.username === owner
+    const history = useHistory();
+
+
+    const handleEdit = () => {
+        history.push(`/posts/${id}/edit`)
+    }
+
+
+    const handleDelete = async () => {
+        try {
+            await axiosRes.delete(`/posts/${id}/`)
+            history.goBack()
+        } catch (err) {
+            console.log(err)
+        }
+    }
 
     const handleLike = async () => {
         try {
@@ -61,16 +79,6 @@ const Post = (props) => {
     };
 
 
-
-
-
-
-
-
-
-
-
-
     return <Card className={styles.Post}>
         <Card.Body>
             <Media className="align-items-center justify-content-between">
@@ -80,7 +88,7 @@ const Post = (props) => {
                 </Link>
                 <div className="d-flex align-items-center">
                     <span>{updated_at}</span>
-                    {is_owner && postPage && "..."}
+                    {is_owner && postPage && <MoreDropdown handleEdit={handleEdit} handleDelete={handleDelete}/>}
                 </div>
             </Media>
         </Card.Body>
